0312f3aca32bc21254e0dfeeac0b029df2e337f4
Asim Yirmili <ayirmili@k1m06s01.42istanbul.com.tr>
2024-10-17 14:16:20 +0300

    SıgQuit done. Norm and File Confs Done.

--- free/free.c
+++ free/free.c
@@ -3,21 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   free.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: beyza <beyza@student.42.fr>                +#+  +:+       +#+        */
+/*   By: ayirmili <ayirmili@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 18:50:09 by beyza             #+#    #+#             */
-/*   Updated: 2024/10/15 17:46:50 by beyza            ###   ########.fr       */
+/*   Updated: 2024/10/17 14:05:38 by ayirmili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+void	free_tmp(void *tmp)
+{
+	if (tmp != NULL)
+	{
+		free(tmp);
+		tmp = NULL;
+	}
+}
 
 void	exit_shell(t_data *data, int exno)
 {
 	if (data)
 	{
 		if (data->cmd && data->cmd->io_fds)
-            ;
+			;
 			//TODO close_fds(data->cmd, true);
 		//TODOfree_data(data, true);
 	}
